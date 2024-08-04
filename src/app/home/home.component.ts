96830fbc57ee3d27472cc011c48d88814e5f7a41
Rezwan66 <shaikhrezwan66@gmail.com>
2024-08-04 21:13:40 +0200

    loaded data into the homepage category sliders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,11 @@ import { MainCarouselComponent } from './main-carousel/main-carousel.component';
 import { ProductSliderComponent } from './product-slider/product-slider.component';
 import { menJeans } from '../../Data/Men/men_jeans';
 import { gounsPage1 } from '../../Data/Gouns/gouns';
-import { lengha_page1 } from '../../Data/Women/LenghaCholi';
-import { kurtaPage1 } from '../../Data/Kurta/kurta';
+// import { lengha_page1 } from '../../Data/Women/LenghaCholi';
+// import { kurtaPage1 } from '../../Data/Kurta/kurta';
 import { mensShoesPage1 } from '../../Data/shoes';
+import { mens_kurta } from '../../Data/Men/men_kurta';
+import { lehngacholiPage2 } from '../../Data/Saree/lenghaCholiPage2';
 
 @Component({
   selector: 'app-home',
@@ -23,8 +25,8 @@ export class HomeComponent {
   ngOnInit() {
     this.menJeans = menJeans.slice(0, 5);
     this.womenGowns = gounsPage1.slice(0, 5);
-    this.lehengaCholi = lengha_page1.slice(0, 5);
-    this.mensKurta = kurtaPage1.slice(0, 5);
+    this.lehengaCholi = lehngacholiPage2.slice(0, 5);
+    this.mensKurta = mens_kurta.slice(0, 5);
     this.shoes = mensShoesPage1.slice(0, 5);
   }
 }
