e0f66fb021267b4b3ee7dc2cc15e2eaab16d1af4
Rezwan66 <shaikhrezwan66@gmail.com>
2024-08-04 20:55:33 +0200

    created card for the home sliders

--- src/app/home/home-product-card/home-product-card.component.ts
+++ src/app/home/home-product-card/home-product-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-product-card',
   standalone: true,
   imports: [],
   templateUrl: './home-product-card.component.html',
-  styleUrl: './home-product-card.component.scss'
+  styleUrl: './home-product-card.component.scss',
 })
 export class HomeProductCardComponent {
-
+  @Input() product: any;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { MainCarouselComponent } from './main-carousel/main-carousel.component';
 import { ProductSliderComponent } from './product-slider/product-slider.component';
+import { menJeans } from '../../Data/Men/men_jeans';
+import { gounsPage1 } from '../../Data/Gouns/gouns';
+import { lengha_page1 } from '../../Data/Women/LenghaCholi';
+import { kurtaPage1 } from '../../Data/Kurta/kurta';
+import { mensShoesPage1 } from '../../Data/shoes';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +14,17 @@ import { ProductSliderComponent } from './product-slider/product-slider.componen
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  menJeans: any;
+  womenGowns: any;
+  lehengaCholi: any;
+  mensKurta: any;
+  shoes: any;
+  ngOnInit() {
+    this.menJeans = menJeans.slice(0, 5);
+    this.womenGowns = gounsPage1.slice(0, 5);
+    this.lehengaCholi = lengha_page1.slice(0, 5);
+    this.mensKurta = kurtaPage1.slice(0, 5);
+    this.shoes = mensShoesPage1.slice(0, 5);
+  }
+}
