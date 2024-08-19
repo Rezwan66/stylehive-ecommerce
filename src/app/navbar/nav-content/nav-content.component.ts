56df9bbe1d52038474460f10545bc850340cedef
Rezwan66 <shaikhrezwan66@gmail.com>
2024-08-19 19:57:09 +0200

    edited nav content

--- src/app/navbar/nav-content/nav-content.component.ts
+++ src/app/navbar/nav-content/nav-content.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { navigation } from './nav-content';
 
 @Component({
   selector: 'app-nav-content',
@@ -11,4 +12,9 @@ import { Component, Input } from '@angular/core';
 export class NavContentComponent {
   category: any;
   @Input() selectedSection: any;
+
+  ngOnInit() {
+    this.category = navigation;
+    console.log('selected section', this.selectedSection);
+  }
 }
